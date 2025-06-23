4298d6c1e66048f4d6e4cd6f4dc286b8c9c7331c
hayasehiise <kuronegahiise@gmail.com>
2025-06-23 16:46:23 +0800

    add favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,14 @@ import tailwindcss from "@tailwindcss/vite";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2025-05-15",
+  app: {
+    head: {
+      title: "Bandara Mutiara SIS Al-Jufri - Bandara Mutiara Palu",
+      htmlAttrs: {
+        lang: "id",
+      },
+    },
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
 
